d17e34286acc27082e6f37f64f06f8d3ebae8bb8
GuilhermeLeati <140661092+GuilhermeLeati@users.noreply.github.com>
2023-09-28 15:34:47 -0700

    Add files via upload

---
+++ pratica js/prog.js
@@ -0,0 +1,66 @@
+alert( "Hello World!")
+
+let a = 1;
+let b = 5;
+let soma = a + b;
+console.log( "A soma é " + soma);
+
+var valor = 47; 
+if (typeof valor === "number") {
+  console.log("É um número");
+} else {
+  console.log("Não é um número");
+}
+
+var string = "Isso é um texto";
+if (typeof string === "string") {
+  console.log("Isso é uma string")
+} else {
+  console.log("Isso não é uma string")
+}
+
+console.log(string)
+
+var trueorfalse = 4;
+if (typeof trueorfalse === "boolean") {
+  console.log("É um boolean");
+} else {
+  console.log("Não é um boolean")
+}
+
+console.log(trueorfalse)
+
+
+let c = 4;
+let d = 9;
+subtração = d - c;
+console.log( "O resultado da subtração é" + subtração);
+
+
+
+let o = 220
+let p = 10
+multiplicação = 220 * 10;
+console.log( "O resultado da multiplicação é " + multiplicação);
+
+
+let e = 220
+let f = 10
+divisão = e / f;
+console.log("O resultado da divisão é " + divisão);
+
+
+
+var par = 22; 
+if ( par % 2 === 0) {
+console.log("É um numero par!");
+} else {
+ console.log("Não é um numero par!");
+}
+
+var impar = 28;
+if (impar % 2 !== 0){
+  console.log("É um numero impar!");
+} else{
+  console.log("Não é um numero impar");
+}
